9276d8e39e548decbdbe73ed7e73a2b20b4a92b0
Ipsky <yap@usc.edu>
2021-08-20 13:57:42 -0700

    formatting

--- index.js
+++ index.js
@@ -6,26 +6,26 @@
  * @typedef {import('mdast-util-to-markdown').Handle} ToMarkdownHandle
  */
 
-import {containerPhrasing} from 'mdast-util-to-markdown/lib/util/container-phrasing.js'
+import { containerPhrasing } from 'mdast-util-to-markdown/lib/util/container-phrasing.js'
 
 /** @type {FromMarkdownExtension} */
 export const gfmStrikethroughFromMarkdown = {
   canContainEols: ['delete'],
-  enter: {strikethrough: enterStrikethrough},
-  exit: {strikethrough: exitStrikethrough}
+  enter: { strikethrough: enterStrikethrough },
+  exit: { strikethrough: exitStrikethrough }
 }
 
 /** @type {ToMarkdownExtension} */
 export const gfmStrikethroughToMarkdown = {
-  unsafe: [{character: '~', inConstruct: 'phrasing'}],
-  handlers: {delete: handleDelete}
+  unsafe: [{ character: '~', inConstruct: 'phrasing' }],
+  handlers: { delete: handleDelete }
 }
 
 handleDelete.peek = peekDelete
 
 /** @type {FromMarkdownHandle} */
 function enterStrikethrough(token) {
-  this.enter({type: 'delete', children: []}, token)
+  this.enter({ type: 'delete', children: [] }, token)
 }
 
 /** @type {FromMarkdownHandle} */
@@ -39,7 +39,7 @@ function exitStrikethrough(token) {
  */
 function handleDelete(node, _, context) {
   const exit = context.enter('emphasis')
-  const value = containerPhrasing(node, context, {before: '~', after: '~'})
+  const value = containerPhrasing(node, context, { before: '~', after: '~' })
   exit()
   return '~~' + value + '~~'
 }
