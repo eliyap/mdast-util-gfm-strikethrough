4b17fcc7503b474d0a58861a73313601b5a7d34b
Ipsky <yap@usc.edu>
2021-08-20 13:23:57 -0700

    draft system

--- mark.ts
+++ mark.ts
@@ -1,7 +1,16 @@
 import { Parent, PhrasingContent } from 'mdast';
+import {
+    Extension as FromMarkdownExtension,
+    Handle as FromMarkdownHandle,
+} from 'mdast-util-from-markdown';
+import {
+    Options as ToMarkdownExtension,
+    Handle as ToMarkdownHandle,
+} from 'mdast-util-to-markdown';
+import { containerPhrasing } from 'mdast-util-to-markdown/lib/util/container-phrasing.js'
 
 export interface Mark extends Parent {
-    type: 'delete';
+    type: 'mark';
     children: PhrasingContent[];
 }
 
@@ -9,4 +18,39 @@ declare module 'mdast' {
     interface StaticPhrasingContentMap {
         mark: Mark;
     }
+}
+
+handleMark['peek'] = peekMark
+
+/** @type {FromMarkdownHandle} */
+function enterMark(token) {
+    this.enter({ type: 'mark', children: [] }, token)
+}
+
+/** @type {} */
+const exitMark: FromMarkdownHandle = function (token) {
+    this.exit(token)
+}
+
+var handleMark: ToMarkdownHandle = function (node: Mark, _, context) {
+    const exit = context.enter('emphasis')
+    const value = containerPhrasing(node, context, { before: '=', after: '=' })
+    exit()
+    return '==' + value + '=='
+}
+
+/** @type {} */
+var peekMark: ToMarkdownHandle = function () {
+    return '=='
+}
+
+export const pandocMarkFromMarkdown: FromMarkdownExtension = {
+    canContainEols: ['mark'],
+    enter: { mark: enterMark },
+    exit: { mark: exitMark }
+}
+
+export const pandocMarkToMarkdown: ToMarkdownExtension = {
+    unsafe: [{ character: '=', inConstruct: 'phrasing' }],
+    handlers: { mark: handleMark }
 }
